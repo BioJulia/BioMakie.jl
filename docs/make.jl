aa524503ee633dc7766c4252b7004d305c345485
Daniel Kool <kool7d@gmail.com>
2020-08-24 02:09:12 -0500

    doc1

--- docs/make.jl
+++ docs/make.jl
@@ -1,20 +1,18 @@
 using Documenter, Literate
 using AbstractPlotting, Makie, MakieLayout
-# GENERATED = joinpath(@__DIR__, "src", "literate")
-# SOURCE_FILES = joinpath.(GENERATED, ["examples.jl"])
-# foreach(fn -> Literate.markdown(fn, GENERATED), SOURCE_FILES)
 
 makedocs(
     sitename="BioMakie",
-    format=Documenter.HTML(),
+    # modules = [BioMakie],
+    # format=Documenter.HTML(),
     pages = Any[
-                "index.md",
+                "Home" => "index.md",
                 "Examples" => "literate/examples.md",
-               ],
-    repo="https://github.com/kool7d/BioMakie.jl.git"
+    ],
+    # repo="https://github.com/kool7d/BioMakie.jl.git"
 )
 
-deploydocs(
-    repo="github.com/kool7d/BioMakie.jl.git"
-)
+# deploydocs(
+    # repo="github.com/kool7d/BioMakie.jl.git"
+# )
 # include("make.jl")
