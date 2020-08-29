db973a845185f469ca156c975c2cc07b52ee294a
Dan Kool <kool7d@gmail.com>
2020-08-28 20:45:39 -0500

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -4,21 +4,18 @@ Pkg.activate(@__DIR__)
 using Documenter, BioMakie
 
 push!(LOAD_PATH, "../src/")
-const CI = get(ENV, "CI", nothing) == "true"
 
 makedocs(
     sitename = "BioMakie",
     modules = [BioMakie],
-    format=Documenter.HTML(prettyurls = CI),
+    format=Documenter.HTML(),
     pages = Any[
                 "Home" => "index.md"
                 ],
 )
 
-if CI
-    deploydocs(
-        repo="github.com/kool7d/BioMakie.jl.git",
-        target = "build",
-        push_preview = true,
+deploydocs(
+    repo="github.com/kool7d/BioMakie.jl.git",
+    target = "build",
+    push_preview = true,
     )
-end
