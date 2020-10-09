f6d2db1853e8708856a4da314de1aee8fa74caaa
Daniel Kool <kool7d@gmail.com>
2020-10-09 18:34:24 -0500

    md10

--- docs/make.jl
+++ docs/make.jl
@@ -5,15 +5,11 @@ using Documenter, BioMakie
 makedocs(
     sitename="BioMakie",
     modules = [BioMakie],
+    build = "",
     format = Documenter.HTML(),
     pages = Any["Home" => "index.md",
-                "Page1" => "page1.md"],
-)
-using Documenter: DeployDecision
-DeployDecision(all_ok = true,
-    repo = "github.com/kool7d/BioMakie",
-    subfolder = "docs",
-    branch = "gh-pages")
+                "Page1" => "page1.md"])
+
 deploydocs(
     repo = "github.com/kool7d/BioMakie",
     target = "docs",
---
+++ docs/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"page1/","page":"Page1","title":"Page1","text":"testing","category":"page"},{"location":"","page":"Home","title":"Home","text":"CurrentModule = BioMakie","category":"page"},{"location":"#BioMakie","page":"Home","title":"BioMakie","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BioMakie]","category":"page"}]
+}
