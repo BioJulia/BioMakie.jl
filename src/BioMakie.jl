b0be2d2ab6053ae3c0f19d7e4ee6f049963fdb71
Daniel Kool <kool7d@gmail.com>
2020-10-09 15:39:14 -0500

    md3

--- src/BioMakie.jl
+++ src/BioMakie.jl
@@ -1,34 +1,34 @@
 module BioMakie
 
-export viewstruc, viewmsa, StructureView, MSAView
-
-using AbstractPlotting
-using BioSequences
-using BioStructures
-using Colors, ColorSchemes
-using DataStructures
-using DelimitedFiles
-using Distances
-using Distributed
-using FileIO
-using GeometryBasics
-using GLFW, GLMakie
-GLFW.WindowHint(GLFW.FLOATING, 1)
-using JLD2
-using Lazy
-using MIToS
-using MIToS.MSA: AbstractMultipleSequenceAlignment, Stockholm
-using MacroTools
-using SplitApplyCombine
-using TensorCast
-# using WGLMakie
-using AbstractPlotting.MakieLayout
-Node = AbstractPlotting.Node
-
-include("../data/basicdata.jl")
-include("utils.jl")
-include("bonds.jl")
-include("structure.jl")
-include("msa.jl")
+# export viewstruc, viewmsa, StructureView, MSAView
+#
+# using AbstractPlotting
+# using BioSequences
+# using BioStructures
+# using Colors, ColorSchemes
+# using DataStructures
+# using DelimitedFiles
+# using Distances
+# using Distributed
+# using FileIO
+# using GeometryBasics
+# using GLFW, GLMakie
+# GLFW.WindowHint(GLFW.FLOATING, 1)
+# using JLD2
+# using Lazy
+# using MIToS
+# using MIToS.MSA: AbstractMultipleSequenceAlignment, Stockholm
+# using MacroTools
+# using SplitApplyCombine
+# using TensorCast
+# # using WGLMakie
+# using AbstractPlotting.MakieLayout
+# Node = AbstractPlotting.Node
+#
+# include("../data/basicdata.jl")
+# include("utils.jl")
+# include("bonds.jl")
+# include("structure.jl")
+# include("msa.jl")
 
 end # BioMakie
