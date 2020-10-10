192b1563e864c3fc7529efb85270c30f3b7d246f
Daniel Kool <kool7d@gmail.com>
2020-10-09 22:22:04 -0500

    bd6

--- docs/make.jl
+++ docs/make.jl
@@ -3,12 +3,15 @@ Pkg.add("Documenter")
 using Documenter, BioMakie
 
 makedocs(
-    sitename="BioMakie",
+    sitename = "BioMakie",
     modules = [BioMakie],
+    build   = "build",
+    clean   = true,
     format = Documenter.HTML(),
     pages = ["Home" => "index.md",
             "Page1" => "page1.md"])
 
-deploydocs(repo = "github.com/kool7d/BioMakie",
-    branch = "gh-pages"
+deploydocs(
+    repo = "github.com/kool7d/BioMakie",
+    branch = "gh-pages",
     target = "docs")
