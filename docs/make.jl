f35d0907524f3653c6aa186e965528b2f243419e
Daniel Kool <kool7d@gmail.com>
2020-10-09 22:01:40 -0500

    bd5

--- docs/make.jl
+++ docs/make.jl
@@ -9,6 +9,6 @@ makedocs(
     pages = ["Home" => "index.md",
             "Page1" => "page1.md"])
 
-deploydocs(
-    repo = "github.com/kool7d/BioMakie",
-    branch = "gh-pages")
+deploydocs(repo = "github.com/kool7d/BioMakie",
+    branch = "gh-pages"
+    target = "docs")
