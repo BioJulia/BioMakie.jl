cd19309d14766274b4b3639e9a04d79ec0f81c58
Daniel Kool <kool7d@gmail.com>
2020-10-12 18:22:18 -0500

    dd4

--- docs/src/proteins.jl
+++ docs/src/proteins.jl
@@ -9,16 +9,16 @@ pstruc1 = viewstruc("2VB1")  # opens a 'Scene' and returns a 'StructureView'
 
 # You could also look at multiple structures at once.
 svs = viewstrucs(["2vb1","1lw3"], colors = ["aqua","ele"])
-#md # ![Image of struc](https://github.com/kool7d/BioMakie.jl/blob/master/assets/2vb1.png)
+#md # ![Image of struc](assets/2vb1.png)
 
 # ## Multiple sequence alignments
 
 # You can similarly load a Multiple Sequence Alignment from the Pfam database using its
 # ID like so:
 msa1 = viewmsa("PF00062")  # opens a 'Scene' and returns an 'MSAView'
-#md # ![Image of msa](https://github.com/kool7d/BioMakie.jl/blob/master/assets/pf00062.png)
+#md # ![Image of msa](assets/pf00062.png)
 # An `MSAView` contains the most relevant information about the protein sequences in the
 # alignment along with the `Scene` and `Layout` which comprise the interface.
 
-using Literate #src
-Literate.markdown("src/proteins.jl","src") #src
+# using Literate #src
+# Literate.markdown("src/proteins.jl","src") #src
