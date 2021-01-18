af1b42dbab3fb2d59961f53af7c89bc9fd4c9b72
Daniel Kool <kool7d@gmail.com>
2021-01-18 16:44:34 -0600

    t7

--- src/BioMakie.jl
+++ src/BioMakie.jl
@@ -3,25 +3,16 @@ module BioMakie
 export viewstruc, viewmsa, StructureView, MSAView
 
 using AbstractPlotting
+using BioSequences
 using BioStructures
-using Colors, ColorSchemes
 using DataStructures
 using DelimitedFiles
 using Distances
-using Distributed
 using FileIO
-using GeometryBasics
-using Makie
-using JLD2
-using Lazy
 using MIToS
-using MIToS.MSA: AbstractMultipleSequenceAlignment, Stockholm
-using MacroTools
 using SplitApplyCombine
 using TensorCast
-# using WGLMakie
-using AbstractPlotting.MakieLayout
-Node = AbstractPlotting.Node
+using GLMakie
 
 include("../data/basicdata.jl")
 include("../src/utils.jl")
