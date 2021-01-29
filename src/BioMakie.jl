8c5cfafead80c0849cee42e70192096c3911b0dc
Daniel Kool <kool7d@gmail.com>
2021-01-28 20:41:37 -0600

    retest2

--- src/BioMakie.jl
+++ src/BioMakie.jl
@@ -16,7 +16,7 @@ using SplitApplyCombine
 using TensorCast
 using GLMakie
 # using WGLMakie
-Node = AbstractPlotting.Node
+Node = GLMakie.Node
 
 include("../data/basicdata.jl")
 include("../data/chemdata.jl")
