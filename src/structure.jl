a00282179cea33494688f74dd4824bb1ab68d97c
Daniel Kool <kool7d@gmail.com>
2021-01-31 01:51:03 -0600

    remake struc2

--- src/structure.jl
+++ src/structure.jl
@@ -1,30 +1,30 @@
 """
 	StructureView is an object/container to hold relevant data for convenience.
 
+	All fields except `scene` are `Observables/Nodes`, so they can be easily tracked
+	and used to trigger updates and events.
+
 	Fields:
-		protein			- The protein structure
-		models			- Structure models
+		id 				- PDB ID by default
 		chains			- Structure chains
 		residues		- Structure residues
 		atoms			- Structure atoms
-		figures			- Figures, Axes, and Plots
+		scene			- The scene showing the structure
 """
 mutable struct StructureView
-	protein
-	models
+	id
 	chains
 	residues
 	atoms
-	figures::AbstractArray{Figure}
+	scene
 end
-StructureView(xs::AbstractArray{Node}) = StructureView(xs..., [])
+StructureView(xs::AbstractArray{Node}) = StructureView(xs..., nothing)
 import BioStructures.chains
-for f in (	:protein,
-			:models,
+for f in (	:id,
 			:chains,
 			:residues,
 			:atoms,
-			:figures
+			:scene
 			)
   @eval $(f)(sv::StructureView) = sv.$(f)[]
 end
@@ -46,41 +46,56 @@ atomcolors(atoms; color = "element") =
 atomradii(atoms) = [vanderwaals[element(x)] for x in atoms]
 resids(residues) = resid.(residues)
 resatoms(residues) = BioStructures.atoms.(residues)
+
+"""
+    structureview(prot::ProteinStructure; kwargs...)
+
+Return a StructureView object for `prot`.
+
+### Optional Arguments:
+- dir (String)         				- Directory for PDB structure
+- select (BioStructures selector) 	- Selector to use, default = :standardselector
+- model	(Int)						- Structure model, default = 1
+
+"""
+function structureview( prot::ProteinStructure;
+						dir = "",
+						select = :standardselector,
+						model = 1)
+	#
+	id1 = prot.name[1:end-4]
+	model1 = prot[model]
+	chains1 = BioStructures.chains(model1)
+	residues1 = BioStructures.collectresidues(model1, eval(select))
+	atoms1 = BioStructures.collectatoms(model1, eval(select))
+	return StructureView(  map( X->Node(X),
+								[ id1,
+								  chains1,
+							  	  residues1,
+							  	  atoms1
+								]))
+end
+
 """
     structureview(str::String; kwargs...)
 
 Return a StructureView object with PDB ID `"str"`.
 
 ### Optional Arguments:
-- dir (String)         - Directory for PDB structure, default `""`
-- showbonds (Boolean)  - To display bonds, default `true`
-- colors (String)      - Color set for atoms, default `"element"`
+- dir (String)         				- Directory for PDB structure
+- select (BioStructures selector) 	- Selector to use, default = :standardselector
+- model	(Int)						- Structure model, default = 1
 
 """
-function structureview( prot::ProteinStructure;
+function structureview( str::String;
 						dir = "",
-						select = :standardselector)
-
-	models1 = BioStructures.models(prot)
-	chains1 = BioStructures.chains(prot)
-	residues1 = BioStructures.collectresidues(prot, eval(select))
-	atoms1 = BioStructures.collectatoms(prot, eval(select))
-	return StructureView(  map( X->Node(X),
-								[ prot,
-							  	  models1,
-							  	  chains1,
-							  	  residues1,
-							  	  atoms1
-								]))
-end
-function structureview(str::String;
-						dir = "",
-						select = :standardselector)
+						select = :standardselector,
+						model = 1)
 
 	if length(str) == 4
-		return structureview(retrievepdb(uppercase(str); dir = dir), select = select)
+		return structureview(retrievepdb(uppercase(str); dir = dir), select = select, model = 1)
 	else
-		return structureview(read("$(str)", BioStructures.PDB), select = select)
+		return structureview(read("$(dir)\\$(str)", BioStructures.PDB), select = select, model = 1)
 	end
 
 	return error("something wrong with the `structureview` input")
@@ -92,36 +107,16 @@ end
 Visualize all structures in the array `strs`.
 
 ### Optional Arguments:
-- dir (String)         - Directory of PDB structure, default `""`
-- showbonds (Boolean)  - To display bonds, default `true`
-- color (String)      - Color set for atoms, default `"element"`
-- resolution (Tuple{Int})   - Resolution of the scene, default `(1500, 600)`
+- dir (String)         	- Directory of PDB structure, default `""`
+- show_bonds (Boolean) 	- To display bonds, default `true`
+- show_id (Boolean) 	- To display id, default `true`
+- color (String)       	- Color set for atoms, default `"element"`
 
 """
-# function viewstruc( sv::StructureView;
-# 					dir = "",
-# 					showbonds = true,
-# 					color = "element",
-# 					resolution = (1200,900)) where T
-# 	#
-# 	fig = GLMakie.Figure(resolution = resolution)
-# 	ax1 = fig[1,1]
-# 	meshscatter(ax1, lift(atomcoords,sv.atoms);
-# 	color = lift(X->atomcolors(X; color = color),sv.atoms),
-# 	markersize = lift(X->(1/3).*atomradii(X),sv.atoms), show_axis = false, resolution = (800,800))
-# 	#
-# 	shp1 = bondshapes.(bonds(residues(sv))) |> collectbondshapes
-# 	bonds1 = normal_mesh.(shp1)
-# 	mesh!(ax1, bonds1[1], color = RGBAf0(0.5,0.5,0.5,0.8))
-# 	for i = 1:size(bonds1,1); mesh!(ax1, bonds1[i], color = RGBAf0(0.5,0.5,0.5,0.8)); end
-#
-# 	display(fig)
-# 	push!(sv.figures,fig)
-# 	return sv
-# end
 function viewstrucs(strs::AbstractArray{T};
 					dir = "",
-					showbonds = true,
+					show_bonds = true,
+					show_id = true,
 					color = "element") where T
 	#
 	len = length(strs)
@@ -135,23 +130,29 @@ function viewstrucs(strs::AbstractArray{T};
 	fig = GLMakie.Figure(resolution = flexres)
 
 	for (i,sv) in enumerate(svs)
-		ax = LScene(fig[1,i])
-		meshscatter!(ax, lift(atomcoords,sv.atoms);
+		sc = LScene(fig[1:8,i], resolution = (400,650))
+		meshscatter!(sc, lift(atomcoords,sv.atoms);
 			color = lift(X->atomcolors(X; color = color),sv.atoms),
-			markersize = lift(X->(1/3).*atomradii(X),sv.atoms), show_axis = false, resolution = (400,650))
+			markersize = lift(X->(1/3).*atomradii(X),sv.atoms), show_axis = false)
 		#
-		shp1 = bondshapes.(bonds(residues(sv))) |> collectbondshapes
-		bonds1 = normal_mesh.(shp1)
-		mesh!(ax, bonds1[1], color = RGBAf0(0.5,0.5,0.5,0.8))
-		for i = 1:size(bonds1,1); mesh!(ax, bonds1[i], color = RGBAf0(0.5,0.5,0.5,0.8)); end
-		push!(sv.figures,fig)
+		if show_bonds == true
+			shp1 = bondshapes.(bonds(residues(sv))) |> collectbondshapes
+			bonds1 = normal_mesh.(shp1)
+			mesh!(sc, bonds1[1], color = RGBAf0(0.5,0.5,0.5,0.8))
+			for i = 1:size(bonds1,1); mesh!(sc, bonds1[i], color = RGBAf0(0.5,0.5,0.5,0.8)); end
+		end
+		sv.scene = sc
+	end
+	if show_id == true
+		for (i,sv) in enumerate(svs)
+			GLMakie.Label(fig[1,i], sv.id, tellwidth = false)
+		end
 	end
 	display(fig)
 
     if len == 1
         return svs[1]
     end
-
 	return svs
 end
 """
@@ -172,8 +173,3 @@ viewstrucs(str::String; kwargs...) = viewstrucs([str]; kwargs...)
 viewstrucs(stv::StructureView; kwargs...) = viewstrucs([stv]; kwargs...)
 viewstrucs(stv::ProteinStructure; kwargs...) = viewstrucs([structureview(stv)]; kwargs...)
 viewstrucs(stvs::AbstractArray{ProteinStructure}; kwargs...) = viewstrucs([structureview.(stvs)...]; kwargs...)
-
-sv1 = structureview("2vb1")
-
-vs1 = viewstruc(sv1)
-vs2 = viewstrucs(["2vb1","1lw3","2vb1"])
