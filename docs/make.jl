74972eb2b8fca73bffa3aea961bcd2d96be310ce
DrWatson <no@mail>
2021-03-05 00:21:11 -0600

    trydoc5

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,10 @@
 using Documenter, BioMakie
 
-makedocs(
+makedocs(repo = "github.com/kool7d/BioMakie",
     sitename = "BioMakie",
     modules = [BioMakie],
     build   = "build",
     clean   = true,
     format = Documenter.HTML())
 
-deploydocs(
-    repo = "github.com/kool7d/BioMakie",
-    branch = "gh-pages",
-    target = "build")
+deploydocs(repo = "github.com/kool7d/BioMakie")
