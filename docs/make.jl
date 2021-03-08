43af4971e346c1f7742d9ece985f377bdfdb4285
kool7d <kool7d@gmail.com>
2021-03-08 01:05:05 -0600

    trydoc7

--- docs/make.jl
+++ docs/make.jl
@@ -5,6 +5,8 @@ makedocs(repo = "github.com/kool7d/BioMakie",
     modules = [BioMakie],
     build   = "build",
     clean   = true,
-    format = Documenter.HTML())
+    format = Documenter.HTML(),
+    pages = Any["Home" => "index.md",
+                "Proteins" => "proteins.md"])
 
 deploydocs(repo = "github.com/kool7d/BioMakie")
