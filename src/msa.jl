5639e4f3b011fb7ef05d91402e255f111136b703
kool7d <kool7d@gmail.com>
2021-05-10 00:40:52 -0500

    addbang

--- src/msa.jl
+++ src/msa.jl
@@ -97,7 +97,7 @@ positions ----- Residue positions, Default - length of msa
 """
 function viewmsa(   msa::Vector{Tuple{String,String}};
 					sheetsize = [20,40],
-					resolution = (1500, 600),
+					resolution = (1000, 400),
                     colorscheme = :viridis,
                     colorval = 2,
                     positions = 0
@@ -156,12 +156,12 @@ function viewmsa(   msa::Vector{Tuple{String,String}};
     on(numsshow) do ns
     	ax1.xticks = (widthrange, ns)
     end
-    ax1.xticklabelsize = 9
-    ax1.yticklabelsize = 13
+    ax1.xticklabelsize = 7.5
+    ax1.yticklabelsize = 10
     ax1.xzoomlock[] = true
     ax1.yzoomlock[] = true
-    axisaspect = 2.3
-    ax1.yticklabelspace[] = 10
+    axisaspect = 2.0
+    ax1.yticklabelspace[] = 6
 
     poly!(ax1, [FRect2D(x, y, 1, 1) for x in 0:(width1-1) for y in 0:(height1-1)],
     	 	color = :transparent, strokecolor = :black, strokewidth = 1)
@@ -172,7 +172,96 @@ function viewmsa(   msa::Vector{Tuple{String,String}};
     scatter!(ax1,
             points1,
             marker = charvec,
-            markersize = (10.0,11.0),
+            markersize = (7.0,8.0),
+    		color = :black,
+    		strokecolor = :black
+            )
+    heatmap!(ax1, msashow, show_grid = true, show_axis = true,
+           colormap = colorscheme
+           )
+    ax1.attributes.xaxisposition[] = :top
+    deregister_interaction!(fig.current_axis.x,:rectanglezoom)
+    return fig
+end
+function viewmsa!(  fig::Figure,
+                    msa::Vector{Tuple{String,String}};
+					sheetsize = [20,40],
+					resolution = (1000, 400),
+                    colorscheme = :viridis,
+                    colorval = 2,
+                    positions = 0
+				)
+    #
+    if positions == 0
+        positions = [1:length(msa[1][2])...]
+    end
+    f2 = [msa[i][2] for i in 1:size(msa,1)]
+    mat = [[f2[i]...] for i in 1:size(f2,1)] |> combinedims .|> string |> _t
+    if size(mat,1) < sheetsize[1]
+        height1 = size(mat,1) - 1
+    else
+        height1 = sheetsize[1]
+    end
+    if size(mat,2) < sheetsize[2]
+        width1 = size(mat,2) - 1
+    else
+        width1 = sheetsize[2]
+    end
+
+    # fig = Figure(resolution = resolution)
+    ax1 = Axis(fig[(end+1):(end+7),3:9])
+    tightlimits!(ax1)
+    labels = Node([msa[i][1] for i in 1:size(msa,1)])
+    nums = Node(positions)
+    labelssize = @lift size($labels,1) - (height1-1)
+    labelsrange = @lift $labelssize:-1:1
+    numssize = @lift size($nums,1) - (width1-1)
+    numsrange = @lift 1:1:$numssize
+
+    sl1 = Slider(fig[end+1,3:9], range = numsrange, startvalue = 1)
+    sl1.value = 1
+    sl2 = Slider(fig[(end-7):end,10], range = labelsrange, startvalue = 1, horizontal = false,
+    	tellwidth = true, height = nothing)
+    sl2.value = 1
+    colorval = Node(colorval)
+    strmsa = mat
+    strmsavals = [ kdict(i) for i in strmsa ]
+    strmsavals2 = strmsavals |> combinedims
+    labelshow = lift(X->labels[][(X+(height1-1):-1:X)],sl2.value)
+    numsshow = lift(X->nums[][(X:1:X+(width1-1))],sl1.value)
+    labelshow2 = lift(X->(X+(height1-1):-1:X),sl2.value)
+    numsshow2 = lift(X->(X:1:X+(width1-1)),sl1.value)
+    fixtmsa = lift(X->replace(strmsavals2[X,:,:], nothing => 0.0),colorval)
+    msashow = @lift $fixtmsa[$labelshow2,$numsshow2] |> _t
+    charmsa = [x[1] for x in strmsa]
+    charshow = @lift charmsa[$labelshow2,$numsshow2]
+    widthrange = indexshift([1:width1...],-0.5)
+    heightrange = indexshift([1:height1...],-0.5)
+    ax1.yticks = (heightrange, labelshow[])
+    on(labelshow) do ls
+    	ax1.yticks = (heightrange, ls)
+    end
+    ax1.xticks = (widthrange, numsshow[])
+    on(numsshow) do ns
+    	ax1.xticks = (widthrange, ns)
+    end
+    ax1.xticklabelsize = 7.5
+    ax1.yticklabelsize = 10
+    ax1.xzoomlock[] = true
+    ax1.yzoomlock[] = true
+    axisaspect = 2.0
+    ax1.yticklabelspace[] = 6
+
+    poly!(ax1, [FRect2D(x, y, 1, 1) for x in 0:(width1-1) for y in 0:(height1-1)],
+    	 	color = :transparent, strokecolor = :black, strokewidth = 1)
+
+    points1 = [Point2f0(x,y) for x in widthrange for y in heightrange] |> collect
+    charvec = @lift SplitApplyCombine.flatten($charshow)
+
+    scatter!(ax1,
+            points1,
+            marker = charvec,
+            markersize = (7.0,8.0),
     		color = :black,
     		strokecolor = :black
             )
--- src/structure.jl
+++ src/structure.jl
@@ -76,7 +76,40 @@ function viewstruc( struc::T,
     colr = lift(X->atomcolors(X; color = atmcolors),atms)
     marksize = lift(X->(atmscale).*atomradii(X),atms)
     fig = Figure(resolution = resolution)
-    ly = fig[1:10,1]
+    ly = fig[1:13,1:10]
+    plt = meshscatter(ly, atmcords; show_axis = false, color = colr, markersize = marksize)
+    if show_bonds == true
+        resshps = @lift bondshape(SplitApplyCombine.flatten(bonds(collectresidues($struc,selectors...))))
+		bbshps = @lift bondshape(SplitApplyCombine.flatten(backbonebonds.(collectchains($struc))))
+        resbnds = @lift normal_mesh.($resshps)
+		bckbnds = @lift normal_mesh.($bbshps)
+        mesh!(ly, resbnds, color = RGBAf0(0.5,0.5,0.5,0.8))
+		mesh!(ly, bckbnds, color = RGBAf0(0.5,0.5,0.5,0.8))
+    end
+    fig
+end
+function viewstruc!(fig::Figure,
+					struc::T,
+					selectors = [standardselector];
+					dir = "",
+					resolution = (800,800),
+					show_bonds = true,
+					atmcolors = "element",
+					atmscale = 1/3
+					) where {T}
+    if !(T<:Node)
+        if T<:String
+            struc = Node(pdbS(struc))
+        else
+            struc = Node(struc)
+        end
+    end
+    atms = @lift BioStructures.collectatoms($struc,selectors...)
+    atmcords = @lift atomcoords($atms)
+    colr = lift(X->atomcolors(X; color = atmcolors),atms)
+    marksize = lift(X->(atmscale).*atomradii(X),atms)
+    # fig = Figure(resolution = resolution)
+    ly = fig[(end+1):(end+13),:]
     plt = meshscatter(ly, atmcords; show_axis = false, color = colr, markersize = marksize)
     if show_bonds == true
         resshps = @lift bondshape(SplitApplyCombine.flatten(bonds(collectresidues($struc,selectors...))))
