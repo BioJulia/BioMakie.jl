c658a781d1653a1b20a4f5746b44e0292d6623ef
kool7d <kool7d@gmail.com>
2021-08-13 00:21:16 -0500

    ex1

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,11 @@
-using BioMakie, Documenter, Literate
+using Documenter, BioMakie
 
 makedocs(; modules=[BioMakie], authors="Dan Kool",
-    repo="https://github.com/kool7d/BioMakie.jl/blob/{commit}{path}#{line}",
     sitename="BioMakie.jl",
     format=Documenter.HTML(; prettyurls=get(ENV, "CI", "false") == "true",
-                       canonical="https://JuliaPlots.github.io/BioMakie.jl", assets=String[]),
+                       canonical="https://kool7d.github.io/BioMakie.jl/stable", assets=String[]),
 
     pages = ["Home" => "index.md",
-            "Usage" => "usage.md",
-            "API" => "api.md"]
-        )
-
-deploydocs(;repo="github.com/kool7d/BioMakie.jl",
-            push_preview=true)
+             "API" => "API.md"]
+)
+include("make.jl")
