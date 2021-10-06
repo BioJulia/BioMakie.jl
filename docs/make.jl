a26ced8e1c1edffa1c4533e4e600d90fe7f4a705
kool7d <kool7d@gmail.com>
2021-10-06 01:29:22 -0500

    site2

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,37 @@
-using Documenter, BioMakie
+using BioMakie
+using Documenter
+using Literate
+using CairoMakie
+using JSServe
+using NetworkDynamics
+using LayeredLayouts
+using LightGraphs
+using PkgDeps
 
-makedocs(; 
-    modules=[BioMakie], authors="Dan Kool",
-    sitename="BioMakie.jl",
-    format=Documenter.HTML(; prettyurls=get(ENV, "CI", "false") == "true",
-                       canonical="https://kool7d.github.io/BioMakie.jl/stable", assets=String[]),
+DocMeta.setdocmeta!(BioMakie, :DocTestSetup, :(using BioMakie); recursive=true)
 
-    pages = ["Home" => "index.md",
-             "API" => "api.md"]
-)
-deploydocs(
-    repo = "github.com/kool7d/BioMakie.jl.git"
-)
+# generate examples
+examples = [
+    joinpath(@__DIR__, "examples", "plots.jl")
+]
+OUTPUT = joinpath(@__DIR__, "src", "generated")
+isdir(OUTPUT) && rm(OUTPUT, recursive=true)
+mkpath(OUTPUT)
+
+for ex in examples
+    Literate.markdown(ex, OUTPUT)
+end
+
+makedocs(; modules=[BioMakie], authors="Daniel Kool",
+         repo="https://github.com/kool7d/BioMakie.jl/blob/{commit}{path}#{line}",
+         sitename="BioMakie.jl",
+         format=Documenter.HTML(; prettyurls=get(ENV, "CI", "false") == "true",
+                                canonical="https://kool7d.github.io/BioMakie.jl/stable", assets=String[]),
+         pages=["Home" => "index.md",
+                "Examples" => [
+                    "Feature Walkthrough" => "generated/plots.md",
+                ]
+                ])
+
+deploydocs(;repo="github.com/kool7d/BioMakie.jl",
+           push_preview=true)
