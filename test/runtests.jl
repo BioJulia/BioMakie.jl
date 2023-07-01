df142e82b1faee0d2f6c8096048a3ba6112af629
Daniel Kool <kool7d@gmail.com>
2023-06-30 23:21:17 -0500

    add tests and fix inspector

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,35 +22,164 @@ using BioMakie:
 @testset "Structure plotting" begin
     # BioStructures
     # dir = joinpath(tempdir(),"structure")
-    # struc = retrievepdb("2VB1", dir=dir)
-    # chn = struc[1]["A"]
-    # chn_obs = Observable(chn)
-    # resz = collectresidues(struc[1]["A"], standardselector)
-    # atms = collectatoms(struc[1]["A"], standardselector)
-    # struc_obs = Observable(struc)
-    # resz_obs = Observable(resz)
-    # atms_obs = Observable(atms)
+    struc = retrievepdb("2VB1")
+    chn = collectchains(struc[1]["A"])
+    resz = collectresidues(struc[1]["A"], standardselector)
+    atms = collectatoms(struc[1]["A"], standardselector)
+    struc_obs = Observable(struc)
+    chn_obs = Observable(chn)
+    resz_obs = Observable(resz)
+    atms_obs = Observable(atms)
+    @test size(resz_obs[]) == (129,)
+    @test resz_obs[][end].name == "LEU"
+    @test atms_obs[][end].coords == [-6.667, 21.245, 10.848]
 
-    # acolors = atomcolors(resz_obs[]; colors = aquacolors)
-    # @test length(acolors) == 1960
-    # acolors = atomcolors(chn_obs[]; colors = aquacolors)
-    # @test length(acolors) == 2203
-    # acolors_obs = atomcolors(chn_obs; colors = aquacolors)
-    # @test length(acolors_obs[]) == 2203
-    # acolors = atomcolors(resz_obs; colors = aquacolors)
-    # @test acolors[][1] == RGB{Float64}(0.472,0.211,0.499)
-    # acolors = atomcolors(resz_obs[]; colors = aquacolors)
-    # @test acolors[1] == RGB{Float64}(0.472,0.211,0.499)
-    # acolors = atomcolors(chn_obs[]; colors = aquacolors)
-    # @test acolors[1] == RGB{Float64}(0.472,0.211,0.499)
+    atmradii = atomradii(collectatoms(struc_obs[]))
+    @test size(atmradii) == (2203,)
+    @test atmradii[5] == 0.76f0
+    @test atmradii[end] == 0.66f0
+    atmradii = atomradii(collectatoms(chn_obs[]))
+    @test size(atmradii) == (2203,)
+    @test atmradii[5] == 0.76f0
+    @test atmradii[end] == 0.66f0
+    atmradii = atomradii(collectatoms(resz_obs[]))
+    @test size(atmradii) == (1960,)
+    @test atmradii[5] == 0.76f0
+    @test atmradii[end] == 0.31f0
+    atmradii = atomradii(collectatoms(atms_obs[]))
+    @test size(atmradii) == (1960,)
+    @test atmradii[5] == 0.76f0
+    @test atmradii[end] == 0.31f0
+    atmradii = atomradii(collectatoms(struc_obs[]); radiustype = :vdw)
+    @test size(atmradii) == (2203,)
+    @test atmradii[5] == 1.7f0
+    @test atmradii[end] == 1.52f0
+    atmradii = atomradii(collectatoms(chn_obs[]); radiustype = :vdw)
+    @test size(atmradii) == (2203,)
+    @test atmradii[5] == 1.7f0
+    @test atmradii[end] == 1.52f0
+    atmradii = atomradii(collectatoms(resz_obs[]); radiustype = :vdw)
+    @test size(atmradii) == (1960,)
+    @test atmradii[5] == 1.7f0
+    @test atmradii[end] == 1.1f0
+    atmradii = atomradii(collectatoms(atms_obs[]); radiustype = :vdw)
+    @test size(atmradii) == (1960,)
+    @test atmradii[5] == 1.7f0
+    @test atmradii[end] == 1.1f0
 
-    # rcolors = rescolors(resz_obs)
-    # @test rcolors[][1] == :orange
-    # rcolors = rescolors(resz_obs[])
-    # @test rcolors[1] == :orange
-    # # rcolors = rescolors(chn_obs[])    # 
-    # # @test rcolors[1] == :orange
+    atmradius = atomradius(collectatoms(struc_obs[])[5])
+    @test atmradius == 0.76f0
+    atmradius = atomradius(collectatoms(chn_obs[])[5])
+    @test atmradius == 0.76f0
+    atmradius = atomradius(collectatoms(resz_obs[])[5])
+    @test atmradius == 0.76f0
+    atmradius = atomradius(collectatoms(atms_obs[])[5])
+    @test atmradius == 0.76f0
+    atmradius = atomradius(collectatoms(struc_obs[])[5]; radiustype = :vdw)
+    @test atmradius == 1.7f0
+    atmradius = atomradius(collectatoms(chn_obs[])[5]; radiustype = :vdw)
+    @test atmradius == 1.7f0
+    atmradius = atomradius(collectatoms(resz_obs[])[5]; radiustype = :vdw)
+    @test atmradius == 1.7f0
+    atmradius = atomradius(collectatoms(atms_obs[])[5]; radiustype = :vdw)
+    @test atmradius == 1.7f0
 
+    inspectlabel = getinspectorlabel(struc_obs[])
+    str = inspectlabel(1,1,1)
+    @test str[12:19] == "res: LYS"
+    inspectlabel = getinspectorlabel(chn_obs[])
+    str = inspectlabel(1,1,1)
+    @test str[12:19] == "res: LYS"
+    inspectlabel = getinspectorlabel(resz_obs[])
+    str = inspectlabel(1,1,1)
+    @test str[12:19] == "res: LYS"
+    inspectlabel = getinspectorlabel(atms_obs[])
+    str = inspectlabel(1,1,1)
+    @test str[12:19] == "res: LYS"
+    inspectlabel = getinspectorlabel(struc_obs)
+    str = inspectlabel(1,1,1)
+    @test str[12:19] == "res: LYS"
+    inspectlabel = getinspectorlabel(chn_obs)
+    str = inspectlabel(1,1,1)
+    @test str[12:19] == "res: LYS"
+    inspectlabel = getinspectorlabel(resz_obs)
+    str = inspectlabel(1,1,1)
+    @test str[12:19] == "res: LYS"
+    inspectlabel = getinspectorlabel(atms_obs)
+    str = inspectlabel(1,1,1)
+    @test str[12:19] == "res: LYS"
+
+    flabel = firstlabel(inspectlabel)
+    @test flabel[12:19] == "res: LYS"
+
+    atmcolors = atomcolors(struc_obs[])
+    @test size(atmcolors) == (2203,)
+    @test atmcolors[5] == :gray
+    @test atmcolors[end] == :red
+    atmcolors = atomcolors(chn_obs[])
+    @test size(atmcolors) == (2203,)
+    @test atmcolors[5] == :gray
+    @test atmcolors[end] == :red
+    atmcolors = atomcolors(resz_obs[])
+    @test size(atmcolors) == (1960,)
+    @test atmcolors[5] == :gray
+    @test atmcolors[end] == :white
+    atmcolors = atomcolors(atms_obs[])
+    @test size(atmcolors) == (1960,)
+    @test atmcolors[5] == :gray
+    @test atmcolors[end] == :white
+
+    atmcolors = atomcolors(struc_obs[]; colors = aquacolors)
+    @test size(atmcolors) == (2203,)
+    @test atmcolors[5] == RGB{Float64}(0.5,0.5,0.5)
+    @test atmcolors[end] == RGB{Float64}(0.111,0.37,0.999)
+    atmcolors = atomcolors(chn_obs[]; colors = aquacolors)
+    @test size(atmcolors) == (2203,)
+    @test atmcolors[5] == RGB{Float64}(0.5,0.5,0.5)
+    @test atmcolors[end] == RGB{Float64}(0.111,0.37,0.999)
+    atmcolors = atomcolors(resz_obs[]; colors = aquacolors)
+    @test size(atmcolors) == (1960,)
+    @test atmcolors[5] == RGB{Float64}(0.5,0.5,0.5)
+    @test atmcolors[end] == RGB{Float64}(0.65,0.96,0.7)
+    atmcolors = atomcolors(atms_obs[]; colors = aquacolors)
+    @test size(atmcolors) == (1960,)
+    @test atmcolors[5] == RGB{Float64}(0.5,0.5,0.5)
+    @test atmcolors[end] == RGB{Float64}(0.65,0.96,0.7)
+
+    atmcolors = atomcolors(struc_obs)
+    @test size(atmcolors[]) == (2203,)
+    @test atmcolors[][5] == :gray
+    @test atmcolors[][end] == :red
+    atmcolors = atomcolors(chn_obs)
+    @test size(atmcolors[]) == (2203,)
+    @test atmcolors[][5] == :gray
+    @test atmcolors[][end] == :red
+    atmcolors = atomcolors(resz_obs)
+    @test size(atmcolors[]) == (1960,)
+    @test atmcolors[][5] == :gray
+    @test atmcolors[][end] == :white
+    atmcolors = atomcolors(atms_obs)
+    @test size(atmcolors[]) == (1960,)
+    @test atmcolors[][5] == :gray
+    @test atmcolors[][end] == :white
+
+    atmcolors = atomcolors(struc_obs; colors = aquacolors)
+    @test size(atmcolors[]) == (2203,)
+    @test atmcolors[][5] == RGB{Float64}(0.5,0.5,0.5)
+    @test atmcolors[][end] == RGB{Float64}(0.111,0.37,0.999)
+    atmcolors = atomcolors(chn_obs; colors = aquacolors)
+    @test size(atmcolors[]) == (2203,)
+    @test atmcolors[][5] == RGB{Float64}(0.5,0.5,0.5)
+    @test atmcolors[][end] == RGB{Float64}(0.111,0.37,0.999)
+    atmcolors = atomcolors(resz_obs; colors = aquacolors)
+    @test size(atmcolors[]) == (1960,)
+    @test atmcolors[][5] == RGB{Float64}(0.5,0.5,0.5)
+    @test atmcolors[][end] == RGB{Float64}(0.65,0.96,0.7)
+    atmcolors = atomcolors(atms_obs; colors = aquacolors)
+    @test size(atmcolors[]) == (1960,)
+    @test atmcolors[][5] == RGB{Float64}(0.5,0.5,0.5)
+    @test atmcolors[][end] == RGB{Float64}(0.65,0.96,0.7)
+    
     # # MIToS
     # filename = downloadpfam("PF00062"; filename = "$(dir)/PF00062.stockholm.gz")
     # msa1 = MIToS.MSA.read("$(dir)/PF00062.stockholm.gz",Stockholm, generatemapping=true, useidcoordinates=true)
