87bf6300ca1503111cd1f958915c2a800e15845f
Daniel Kool <kool7d@gmail.com>
2023-07-19 23:41:12 -0500

    try fix docs 1

--- docs/make.jl
+++ docs/make.jl
@@ -6,15 +6,7 @@ using MIToS
 using GLMakie
 using GLMakie: Observable
 
-DocMeta.setdocmeta!(BioMakie, :DocTestSetup, :(using BioMakie); recursive=true)
-
-# examples = [
-#     "./src/index.jl",
-# ]
-
-# for ex in examples
-#     Literate.markdown(ex, "src")
-# end
+# DocMeta.setdocmeta!(BioMakie, :DocTestSetup, :(using BioMakie); recursive=true)
 
 makedocs(; modules=[BioMakie], authors="Daniel Kool",
          repo="https://github.com/kool7d/BioMakie.jl/blob/{commit}{path}#{line}",
@@ -22,10 +14,10 @@ makedocs(; modules=[BioMakie], authors="Daniel Kool",
          format=Documenter.HTML(; canonical="https://kool7d.github.io/BioMakie.jl"),
          pages=["Home" => "index.md",
                 "Usage" => "usage.md",
-                "Examples" => "Examples/alphashape.md",
+              #   "Examples" => "Examples/alphashape.md",
                 "API" => "API.md",
-                ]
+                ],
 )
 
 deploydocs(;repo="github.com/kool7d/BioMakie.jl",
-           push_preview=true)
+              push_preview=true)
