1ac170fe4219efe5feb40b7b4ae302f376105495
Daniel Kool <kool7d@gmail.com>
2023-07-21 01:03:11 -0500

    docs1

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,17 @@
-push!(LOAD_PATH,"../src/")
 using Documenter
 using BioMakie
-# using Literate
-# using PkgDeps
-# using MIToS
-# using GLMakie
-# using GLMakie: Observable
 
-# DocMeta.setdocmeta!(BioMakie, :DocTestSetup, :(using BioMakie); recursive=true)
-
-makedocs(; modules=[BioMakie], authors="Daniel Kool",
-         sitename="BioMakie.jl",
-         format=Documenter.HTML(; canonical="https://kool7d.github.io/BioMakie.jl"),
-         pages=["Home" => "index.md",
-                "Usage" => "usage.md",
-              #   "Examples" => "Examples/alphashape.md",
-                "API" => "API.md",
-                ]
+makedocs(
+    sitename = "BioMakie.jl",
+    pages=[
+		"Home" => "index.md",
+		"Usage" => "usage.md",
+		# "Examples" => "Examples/alphashape.md",
+		"API" => "API.md",
+    ]
 )
 
-# deploydocs(;repo="github.com/kool7d/BioMakie.jl",
-#               push_preview=true)
+deploydocs(
+	repo="github.com/kool7d/BioMakie.jl.git",
+    push_preview=true
+)
